348403422cb3bf14751bc11832c2e30afd7d4a35
Oliver Meulengracht <olivermeulengracht@protonmail.com>
2026-01-10 15:44:31 +0000

    Add _codeql_build_dir to .gitignore and remove build artifacts
    Co-authored-by: MeulenG <77005584+MeulenG@users.noreply.github.com>

--- _codeql_build_dir/kernel/output/CMakeFiles/terminal.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "../../terminal.o"
-  "CMakeFiles/terminal"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/terminal.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
