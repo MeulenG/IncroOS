ec35ef624dc276dfe9a7d2d30f88fcd69429a587
Oliver Meulengracht <olivermeulengracht@protonmail.com>
2026-01-11 13:01:23 +0000

    Remove build artifacts from repository and update .gitignore
    Co-authored-by: MeulenG <77005584+MeulenG@users.noreply.github.com>

--- _codeql_build_dir/kernel/drivers/CMakeFiles/SerialDriver.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "../../serial.o"
-  "CMakeFiles/SerialDriver"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/SerialDriver.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
